8529322443574d40a6f80b101d171891bfa4c592
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2021-05-25 12:23:03 +0900

    Update Modified-Kaprekar-Numbers.cpp

--- Modified-Kaprekar-Numbers.cpp
+++ Modified-Kaprekar-Numbers.cpp
@@ -1,1 +1,25 @@
+void kaprekarNumbers(int p, int q)
+{
+    bool b_result = false;
 
+    for (long int i = p; i <= q; ++i)
+    {
+        long int squ = i * i;
+        long int len = (long int)log10(i) + 1;
+        long int po0 = (long int)pow(10, len);
+        long int right = squ % po0;
+        long int left = squ / po0;
+        long int sum = left + right;
+
+        if (sum == i)
+        {
+            cout << sum << " ";
+            b_result = true;
+        }
+    }
+
+    if (b_result == false)
+    {
+        cout << "INVALID RANGE";
+    }
+}
