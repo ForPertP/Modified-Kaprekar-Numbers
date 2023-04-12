77d34721f562dcb6ee61edf3fb3cd958d617696a
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2023-04-12 12:10:35 +0900

    Update Modified-Kaprekar-Numbers.cpp

--- Modified-Kaprekar-Numbers.cpp
+++ Modified-Kaprekar-Numbers.cpp
@@ -38,3 +38,42 @@ void kaprekarNumbers(int p, int q)
         cout << "INVALID RANGE";
     }
 }
+
+int main()
+{
+    string p_temp;
+    getline(cin, p_temp);
+
+    int p = stoi(ltrim(rtrim(p_temp)));
+
+    string q_temp;
+    getline(cin, q_temp);
+
+    int q = stoi(ltrim(rtrim(q_temp)));
+
+    kaprekarNumbers(p, q);
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
